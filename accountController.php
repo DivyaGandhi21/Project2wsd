1a31e49ee6ac114b94403aa3e922c569db30e2cc
DivyaGandhi21 <31937218+DivyaGandhi21@users.noreply.github.com>
2017-12-05 23:21:24 -0500

    added account controller

---
+++ accountController.php
@@ -0,0 +1,99 @@
+<?php
+
+//each page extends controller and the index.php?page=tasks causes the controller to be called
+class accountsController extends http\controller
+{
+    //each method in the controller is named an action.
+    //to call the show function the url is index.php?page=task&action=show
+    public static function show()
+    {
+        $record = accounts::findOne($_REQUEST['id']);
+        self::getTemplate('show_account', $record);
+    }
+    //to call the show function the url is index.php?page=accounts&action=all
+    public static function all()
+    {
+        $records = accounts::findAll();
+        self::getTemplate('all_accounts', $records);
+    }
+    //to call the show function the url is called with a post to: index.php?page=task&action=create
+    //this is a function to create new tasks
+    
+    public static function register()
+    {
+       
+        self::getTemplate('register');
+    }
+    //this is the function to save the user the new user for registration
+    public static function store()
+    {
+        $user = accounts::findUserbyEmail($_REQUEST['email']);
+        if ($user == FALSE) {
+            $user = new account();
+            $user->email = $_POST['email'];
+            $user->fname = $_POST['fname'];
+            $user->lname = $_POST['lname'];
+            $user->phone = $_POST['phone'];
+            $user->birthday = $_POST['birthday'];
+            $user->gender = $_POST['gender'];
+            //$user->password = $_POST['password'];
+            //this creates the password
+            //this is a mistake you can fix...
+            //Turn the set password function into a static method on a utility class.
+            $user->password = $user->setPassword($_POST['password']);
+            $user->save();
+            //you may want to send the person to a
+            // login page or create a session and log them in
+            // and then send them to the task list page and a link to create tasks
+            header("Location: index.php?page=accounts&action=all");
+        } else {
+            //You can make a template for errors called error.php
+            // and load the template here with the error you want to show.
+           // echo 'already registered';
+            $error = 'already registered';
+            self::getTemplate('error', $error);
+        }
+    }
+    public static function edit()
+    {
+        $record = accounts::findOne($_REQUEST['id']);
+        self::getTemplate('edit_account', $record);
+    }
+//this is used to save the update form data
+    public static function save() {
+        $user = accounts::findOne($_REQUEST['id']);
+        $user->email = $_POST['email'];
+        $user->fname = $_POST['fname'];
+        $user->lname = $_POST['lname'];
+        $user->phone = $_POST['phone'];
+        $user->birthday = $_POST['birthday'];
+        $user->gender = $_POST['gender'];
+        $user->save();
+        header("Location: index.php?page=accounts&action=all");
+    }
+    public static function delete() {
+        $record = accounts::findOne($_REQUEST['id']);
+        $record->delete();
+        header("Location: index.php?page=accounts&action=all");
+    }
+    //this is to login, here is where you find the account and allow login or deny.
+    public static function login()
+    {
+       
+        //        $record = accounts::findUser($_POST['email']);
+        $user = accounts::findUserbyEmail($_REQUEST['email']);
+        if ($user == FALSE) {
+            echo 'user not found';
+        } else {
+            if($user->checkPassword($_POST['password']) == TRUE) {
+                echo 'login';
+                session_start();
+                $_SESSION["userID"] = $user->id;
+                //forward the user to the show all todos page
+                print_r($_SESSION);
+            } else {
+                echo 'password does not match';
+            }
+        }
+    }
+}
